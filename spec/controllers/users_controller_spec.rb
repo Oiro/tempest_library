994879f92f618ada3107ea8a200f18f74d636928
Oiro <moses.oiro.okello@gmail.com>
2016-12-09 18:30:46 +0300

    rspec controller user testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe UsersController, type: :controller do
     #expect(response).to have_http_status(:created)
 
   end
+
+  it "creates new user" do
+
+   #post :create, { :user => { :email => "moses@community.lakehub.co.ke"  }  }
+   post:create, params: { user: {name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty"}  }
+   
+   # confirm the user is created
+   # User.create(email: 'moses@community.lakehub.co.ke') 
+   expect(@user).to eq @user
+   # confirm the flash is showing
+   expect(flash[:notice]).to have_content("Account successfully created")
+
+   # confirms redirect_to whichever the page
+   #expect(subject).to redirect_to :action => :show
+                                          
+   expect(subject).to redirect_to(users_url)
+  end
 end
