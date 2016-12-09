66de6b9e58c4579ba05dc43887032420c5bce69a
Oiro <moses.oiro.okello@gmail.com>
2016-12-09 16:23:47 +0300

    rspec controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  it "it renders http success" do
+
+      get :index
+
+      expect(response).to have_http_status(:success)
+            
+            
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  it "assigns user" do
+
+    get :new
+    
+    expect(assigns(:user)).to be_a_new(User)
+    #expect(response).to render_template(:new)
+    #expect(response).to have_http_status(:created)
+
+  end
 end
