3a456109770087e624fa018858c6e9a057f3b154
Oiro <moses.oiro.okello@gmail.com>
2016-12-09 19:07:45 +0300

    rspec controller user testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UsersController, type: :controller do
     get :new
     
     expect(assigns(:user)).to be_a_new(User)
-    #expect(response).to render_template(:new)
+    expect(response).to render_template(:new)
     #expect(response).to have_http_status(:created)
 
   end
