efe9493fda3a9891db67fbd26ac63c3bf64cd0bf
Oiro <moses.oiro.okello@gmail.com>
2016-12-09 19:46:13 +0300

    rspec controller user testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,29 +7,57 @@ RSpec.describe UsersController, type: :controller do
     get :new
     
     expect(assigns(:user)).to be_a_new(User)
-    #expect(response).to render_template(:new)
+    expect(response).to render_template(:new)
     #expect(response).to have_http_status(:created)
 
   end
 
   it "creates new user" do
   
-   before_count = User.count
+   #before_count = User.count
    #post :create, { :user => { :email => "moses@community.lakehub.co.ke"  }  }
-   post:create, params: { user: {name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty"}  }
+   #post :create, params: { user: {name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty"}  }
    
    # confirm the user is created
    # User.create(email: 'moses@community.lakehub.co.ke') 
    # expect(@user).to eq @user
-   expect(User.count).not_to eq(before_count)
+   # expect(User.count).not_to eq(before_count)
    # confirm the flash is showing
-   expect(flash[:notice]).to have_content("Account successfully created")
+   #expect(flash[:notice]).to have_content("Account successfully created")
 
    # confirms redirect_to whichever the page
    #expect(subject).to redirect_to :action => :show
                                           
    #expect(subject).to redirect_to(users_url)
-   expect(response).to redirect_to(users_url) 
+   #expect(response).to redirect_to(users_url) 
   
+  end
+
+  context "POST#CREATE" do
+
+  it "saves user to the database" do
+     
+      before_count = User.count
+
+      post :create, params: { user: { name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty" } }
+
+      expect(User.count).not_to eq(before_count)
+
+  end
+
+   it "shows the correct flash" do
+
+      post :create, params: { user: { name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty" } }
+   
+      expect(flash[:notice]).to have_content("Account successfully created")
+   end
+
+  it "redirects to the user index " do
+
+      post :create, params: { user: { name: "Moses Okello", email: "moses@community.lakehub.co.ke", password: "qwerty", password_confirm: "qwerty" } }
+      expect(response).to redirect_to(users_url)
+  
+  end
+
   end
 end
