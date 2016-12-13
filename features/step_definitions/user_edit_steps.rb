889fc4d2d865d4bef5419061a21d8a43601ee83b
Oiro <moses.oiro.okello@gmail.com>
2016-12-13 08:52:19 +0300

    edit feature and steps

--- features/step_definitions/user_edit_steps.rb
+++ features/step_definitions/user_edit_steps.rb
@@ -1,23 +1,16 @@
 Given(/^am a registered user$/) do
-   # get '/edit'
-    #expect(response).to render_template(:edit)
     visit edit_path
 end
 
 When(/^I visit edit page$/) do
-    #user = FactoryGirl.create(:user)
-    #get :edit user.id
     visit edit_path
 end
 
-And(/^edit the details of the user$/) do
-    # user = FactoryGirl.create(:user)
-    # get '/edit', id: @user
+And(/^I should edit my details$/) do
+
 end
 
-And(/^I should update the details$/) do
-    #expect(response).to redirect_to(@user)
-    #User.create(:user)
+And(/^I should update my  details$/) do
 end
 
 
