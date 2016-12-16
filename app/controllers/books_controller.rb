ba7cebbac4b9cb520c9840c4b81a05f6116a42c9
Oiro <moses.oiro.okello@gmail.com>
2016-12-16 17:39:38 +0300

    book controller and new changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,23 +2,26 @@ class BooksController < ApplicationController
  def index
    @books = Book.all
  end
+ def show
+   @book = Book.find_by(params[:id])
+ end
   
   def new
     @book = Book.new
   end
-
+  def edit
+     @book = Book.find_by(params[:id])
+  end
+  
   def create
     #@book = Book.new(params[:books])
     #@book.save
     @book = Book.new(book_params)
-          if @book.save
-             redirect_to books_url , notice:  'Book successfully added'
-             else
-              render 'new'
-             end
-  end
-  def edit
-    @book = Book.find_by(params[:id])
+     if @book.save
+        redirect_to books_url , notice:  'Book successfully added'
+     else
+        render 'new'
+     end
   end
   def update
   @book = Book.find_by(params[:id])
@@ -33,5 +36,5 @@ class BooksController < ApplicationController
   private
   def book_params
       params.require(:book).permit(:title, :isbn, :user_id, :quantity)
-end
+  end
 end
